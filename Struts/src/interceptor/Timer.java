0aa480c2c4d053e5223633c7cb2e16aae811a7ef
youngmingwag <ymgwag@gmail.com>
2018-05-10 15:17:41 +0900

    마지막

--- Struts/src/interceptor/Timer.java
+++ Struts/src/interceptor/Timer.java
@@ -2,10 +2,10 @@ package interceptor;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
 
-public class Timer extends AbstractMethodError{
+public class Timer extends AbstractInterceptor{
 	
 	private static Log log=LogFactory.getLog(Timer.class);
 	public String intercept(ActionInvocation invocation)throws Exception{
